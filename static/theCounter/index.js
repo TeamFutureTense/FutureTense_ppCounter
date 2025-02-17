4a7285b736d2921b5c858957950fe6c1828370e0
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2025-02-17 01:54:17 -0500

    Update pp counter updating logic

--- static/theCounter/index.js
+++ static/theCounter/index.js
@@ -14,10 +14,51 @@ const cache = {
 
 
 // Smoouth numbers update
-const accuracy = new CountUp('accuracy', 0, 0, 2, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
-const h100 = new CountUp('h100', 0, 0, 0, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
-const h50 = new CountUp('h50', 0, 0, 0, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
-const h0 = new CountUp('h0', 0, 0, 0, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
+// const accuracy = new CountUp('accuracy', 0, 0, 2, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
+// const h100 = new CountUp('h100', 0, 0, 0, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
+// const h50 = new CountUp('h50', 0, 0, 0, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
+// const h0 = new CountUp('h0', 0, 0, 0, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
+const ingame_currCounter = new CountUp(
+  'ingame_currCounter', 
+  0, 
+  0, 
+  2, 
+  .5, 
+  { 
+    useEasing: true, 
+    useGrouping: true, 
+    separator: " ", 
+    decimal: "." 
+  }
+)
+
+const ingame_totalCounter = new CountUp(
+  'ingame_totalCounter', 
+  0, 
+  0, 
+  2, 
+  .5, 
+  { 
+    useEasing: true, 
+    useGrouping: true, 
+    separator: " ", 
+    decimal: "." 
+  }
+)
+
+const songSelect_totalCounter = new CountUp(
+  'songSelect_totalCounter', 
+  0, 
+  0, 
+  2, 
+  .5, 
+  { 
+    useEasing: true, 
+    useGrouping: true, 
+    separator: " ", 
+    decimal: "." 
+  }
+)
 
 
 
@@ -25,41 +66,14 @@ const h0 = new CountUp('h0', 0, 0, 0, .5, { useEasing: true, useGrouping: true, 
 // receive message update from websocket
 socket.api_v2(({ play }) => {
   try {
-    // check if value has changed
-    if (cache.h100 !== play.hits['100']) {
-      // update cache
-      cache.h100 = play.hits['100'];
-
-
-      //      IMPORTANT   !!   USE ONE OF THEM
-
-      // update html via countup
-      h100.update(play.hits['100']);
-
-      // update html via js
-      document.getElementById('h100').innerHTML = play.hits['100'];
-    };
-
-
-
-    if (cache.h50 !== play.hits['50']) {
-      cache.h50 = play.hits['50'];
-      h50.update(play.hits['50']);
-    };
-
-    if (cache.h0 !== play.hits['0']) {
-      cache.h0 = play.hits['0'];
-      h0.update(play.hits['0']);
-    };
-
-    if (cache.accuracy != play.accuracy) {
-      cache.accuracy = play.accuracy;
-      accuracy.update(play.accuracy);
-    };
-
     if (cache.pp !== Math.round(play.pp.current)) {
       cache.pp = Math.round(play.pp.current);
-      document.getElementById('pp').innerHTML = Math.round(play.pp.current);
+      document.getElementById('ingame_currCounter').innerHTML = Math.round(play.pp.current);
+    };
+    if (cache.pp !== Math.round(play.pp.fc)) {
+      cache.pp = Math.round(play.pp.fc);
+      document.getElementById('ingame_totalCounter').innerHTML = Math.round(play.pp.fc);
+      document.getElementById('songSelect_totalCounter').innerHTML = Math.round(play.pp.fc);
     };
   } catch (error) {
     console.log(error);
